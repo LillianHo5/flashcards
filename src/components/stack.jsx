867d4890a74216b4cc3d78a70184d05b35b43efd
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2023-03-06 23:13:12 -0800

    Updated questions to be neuroscience related

--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -6,9 +6,16 @@ const Stack = () => {
     const [isFlipped, setFlipped] = useState(false);
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
     const cards = [
-        { question: 'hello', answer: 'hi' },
-        { question: 'cheese', answer: 'naur' },
-        { question: 'wot', answer: 'meet' }
+        { question: 'What are the two main divisions of the nervous system?', answer: 'Central Nervous System and Peripheral Nervous System' },
+        { question: 'Which of the following glial cells is found in the peripheral nervous system?', answer: 'Schwann Cells' },
+        { question: 'Which of the following are glial cells of the central nervous system?', answer: 'Astrocytes, Ependymal Cells, Oligodendrocytes, Microglia' },
+        { question: 'What is the input region of the neuron?', answer: 'Dendrite' },
+        { question: 'The influx of sodium causes', answer: 'Depolarization' },
+        { question: 'What are the two components of the central nervous system?', answer: 'Brain and Spinal Cord' },
+        { question: 'The autonomic nervous system innervates what?', answer: 'Smooth Muscles, Cardiac Muscles, Glands' },
+        { question: 'A neuroscientist who studies the effects of drugs on mood and behavior is a...', answer: 'Behavorial Neuroscientist' },
+        { question: 'The immune cells of the central nervous system are:', answer: 'Microglia' },
+        { question: 'If the membrane potential changes from +30 mV to -65 mV, this is called?', answer: 'Repolarization' }
     ];
 
     const flipCard = () => {
