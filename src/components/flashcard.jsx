324fbffa1370dbd41dcdef74dec5eacf5b877090
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2023-03-06 23:51:30 -0800

    Added color based on difficulty rating

--- src/components/flashcard.jsx
+++ src/components/flashcard.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
 const FlashCard = (props) => {
+    const color =
+        props.difficulty === "Hard"
+            ? "#FFD6C9"
+            : props.difficulty === "Normal"
+                ? "#FFF8B8"
+                : "#CAF2C2";
+
     return (
-        <div className={["flashcard", props.isFlipped ? "flipped" : ""]} onClick={props.flip}>
+        <div className={["flashcard", props.isFlipped ? "flipped" : ""]}
+            style={{
+                backgroundColor: color,
+                borderRadius: "1rem"
+            }}
+            onClick={props.flip} >
             <div className="flashcard-inner">
                 {props.isFlipped ?
                     <div className="back">
@@ -13,7 +25,7 @@ const FlashCard = (props) => {
                         <p className="question">{props.question}</p>
                     </div>}
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -6,16 +6,16 @@ const Stack = () => {
     const [isFlipped, setFlipped] = useState(false);
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
     const cards = [
-        { question: 'What are the two main divisions of the nervous system?', answer: 'Central Nervous System and Peripheral Nervous System' },
-        { question: 'Which of the following glial cells is found in the peripheral nervous system?', answer: 'Schwann Cells' },
-        { question: 'Which of the following are glial cells of the central nervous system?', answer: 'Astrocytes, Ependymal Cells, Oligodendrocytes, Microglia' },
-        { question: 'What is the input region of the neuron?', answer: 'Dendrite' },
-        { question: 'The influx of sodium causes', answer: 'Depolarization' },
-        { question: 'What are the two components of the central nervous system?', answer: 'Brain and Spinal Cord' },
-        { question: 'The autonomic nervous system innervates what?', answer: 'Smooth Muscles, Cardiac Muscles, Glands' },
-        { question: 'A neuroscientist who studies the effects of drugs on mood and behavior is a...', answer: 'Behavorial Neuroscientist' },
-        { question: 'The immune cells of the central nervous system are:', answer: 'Microglia' },
-        { question: 'If the membrane potential changes from +30 mV to -65 mV, this is called?', answer: 'Repolarization' }
+        { question: 'What are the two main divisions of the nervous system?', answer: 'Central Nervous System and Peripheral Nervous System', difficulty: 'Hard' },
+        { question: 'Which of the following glial cells is found in the peripheral nervous system?', answer: 'Schwann Cells', difficulty: 'Normal' },
+        { question: 'Which of the following are glial cells of the central nervous system?', answer: 'Astrocytes, Ependymal Cells, Oligodendrocytes, Microglia', difficulty: 'Hard' },
+        { question: 'What is the input region of the neuron?', answer: 'Dendrite', difficulty: 'Easy' },
+        { question: 'The influx of sodium causes', answer: 'Depolarization', difficulty: 'Easy' },
+        { question: 'What are the two components of the central nervous system?', answer: 'Brain and Spinal Cord', difficulty: 'Normal' },
+        { question: 'The autonomic nervous system innervates what?', answer: 'Smooth Muscles, Cardiac Muscles, Glands', difficulty: 'Hard' },
+        { question: 'A neuroscientist who studies the effects of drugs on mood and behavior is a...', answer: 'Behavorial Neuroscientist', difficulty: 'Normal' },
+        { question: 'The immune cells of the central nervous system are:', answer: 'Microglia', difficulty: 'Easy' },
+        { question: 'If the membrane potential changes from +30 mV to -65 mV, this is called?', answer: 'Repolarization', difficulty: 'Normal' }
     ];
 
     const flipCard = () => {
@@ -33,7 +33,9 @@ const Stack = () => {
         <div className="flashcard-stack">
             <h4>Number of flashcards: {cards.length}</h4>
             <FlashCard question={cards[currentCardIndex].question}
-                answer={cards[currentCardIndex].answer} isFlipped={isFlipped} flip={flipCard} />
+                answer={cards[currentCardIndex].answer} isFlipped={isFlipped} flip={flipCard}
+                difficulty={cards[currentCardIndex].difficulty}
+            />
             <div className="buttons">
                 <button onClick={nextCard}>Next</button>
             </div>
